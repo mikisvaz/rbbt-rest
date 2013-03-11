c9e560dea888ffe8b006dde06b2458b0d55a80cd
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-03-11 13:47:47 +0100

    Refactored actions

--- share/views/public/js/actions.js
+++ share/views/public/js/actions.js
@@ -18,9 +18,10 @@ function load_action(link){
     }
     var action_div = action_controller.next('.action_loader').first();
     if (action_div.find('> .action_card > .action_parameters').length > 0){
-      action_controller.find('dd.controls > ul > li.parameters').addClass('active');
+      //action_controller.find('dd.controls > ul > li.parameters').addClass('active');
+      action_controller.find('ul.controls > li.parameters').addClass('active');
     }else{
-      action_controller.find('dd.controls > ul > li.parameters').removeClass('active');
+      action_controller.find('ul.controls > li.parameters').removeClass('active');
     }
   }
 
@@ -47,9 +48,24 @@ function display_parameters(){
 }
 
 function setup_action(){
+  show_actions = function(){
+    var link = $(this);
+    var action_list_item = link.parent('li');
+    var action_list = action_list_item.parent('ul.actions');
+    action_list.addClass('select')
+
+    return false
+  };
+
+
   activate_action = function(){
     var link = $(this);
     load_action(link);
+
+    var action_list_item = link.parent('li');
+    var action_list = action_list_item.parent('ul.actions');
+    action_list.removeClass('select')
+
     return false
   };
 
@@ -57,8 +73,8 @@ function setup_action(){
     var link = $(this);
 
     var action_list_item = link.parent('li');
-    var action_list = action_list_item.parent('ul');
-    var action_controller = action_list.parent('dd').parent('.action_controller');
+    var action_list = action_list_item.parent('ul.controls');
+    var action_controller = action_list.parent('.action_controller');
     var action_div = action_controller.next('.action_loader').first();
 
     if (action_div.attr('target-href') != undefined){
@@ -70,10 +86,15 @@ function setup_action(){
 
   var body = $('body');
 
-  body.on('click', 'dl.action_controller > dd.actions  ul > li > a.entity_list_action', activate_action)
-  body.on('click', 'dl.action_controller > dd.actions  ul > li > a.entity_action', activate_action)
-  body.on('click', 'dl.action_controller > dd.controls > ul > li > a.reload_action', reload_action)
-  body.on('click', 'dl.action_controller > dd.controls > ul > li.parameters > a', display_parameters)
+  //body.on('click', 'dl.action_controller > dd.actions  ul > li > a.entity_list_action', activate_action)
+  //body.on('click', 'dl.action_controller > dd.actions  ul > li > a.entity_action', activate_action)
+  //body.on('click', 'dl.action_controller > dd.controls > ul > li > a.reload_action', reload_action)
+  //body.on('click', 'dl.action_controller > dd.controls > ul > li.parameters > a', display_parameters)
+  body.on('click', '.action_controller > ul.actions  li > a.entity_list_action', activate_action)
+  body.on('click', '.action_controller > ul.actions  li > a.entity_action', activate_action)
+  body.on('click', '.action_controller > ul.actions > li.select > a', show_actions)
+  body.on('click', '.action_controller > ul.controls > li > a.reload_action', reload_action)
+  body.on('click', '.action_controller > ul.controls > li.parameters > a', display_parameters)
 }
 
 
