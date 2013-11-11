f87bf44f8366bd24d36eda2767f335cb16c3a810
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-11-12 00:55:16 +0100

    Encode URI

--- lib/rbbt/rest/client.rb
+++ lib/rbbt/rest/client.rb
@@ -24,20 +24,20 @@ class WorkflowRESTClient
   def self.get_raw(url, params = {})
     Log.debug{ "RestClient get_raw: #{ url } - #{Misc.fingerprint params}" }
     params = params.merge({ :_format => 'raw' })
-    RestClient.get(url, :params => params)
+    RestClient.get(URI.encode(url), :params => params)
   end
  
   def self.post_jobname(url, params = {})
     Log.debug{ "RestClient post_jobname: #{ url } - #{Misc.fingerprint params}" }
     params = params.merge({ :_format => 'jobname' })
-    RestClient.post(url, params)
+    RestClient.post(URI.encode(url), params)
   end
   
   def self.get_json(url, params = {})
     Log.debug{ "RestClient get_json: #{ url } - #{Misc.fingerprint params }" }
     params = params.merge({ :_format => 'json' })
     begin
-      res = RestClient.get(url, :params => params)
+      res = RestClient.get(URI.encode(url), :params => params)
     rescue => e
       raise JSON.parse(e.response)["message"]
     end
@@ -53,7 +53,7 @@ class WorkflowRESTClient
       JSON.parse(Open.open(url, :nocache => true))
     else
       params = params.merge({ :_format => 'json' })
-      res = RestClient.post(url, params)
+      res = RestClient.post(URI.encode(url), params)
       begin
         JSON.parse(res)
       rescue
