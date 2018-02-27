949cbe907d4a7c6c6274c7fdce67f0bac5153644
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2018-02-27 16:19:08 +0100

    nil

--- share/views/public/js/rbbt/reveal.js
+++ share/views/public/js/rbbt/reveal.js
@@ -8,16 +8,18 @@ $.widget("rbbt.reveal", {
    var modal = this.options.modal;
 
    tool.element.click(function(){
-    var modal = $('#modal').first();
     var link = $(this);
     var source_id = link.attr('attr-reveal_id');
     var title     = link.attr('attr-reveal_title');
     var content = $('#' + source_id);
 
+    var modal = tool.options.modal;
+
     var height = window.innerHeight - $('#top_menu').outerHeight(true);
     modal.css('height',height)
     rbbt.modal.controller.show(content.html(), title)
     modal.attr('attr-reveal_source_id', source_id)
+    update_rbbt()
     return false
    })
   },
